df1194eaf6e6cd20dcb73fab1129c397974d005b
cydia-waltz <135396758+cydiawaltz@users.noreply.github.com>
2024-01-12 13:10:32 +0900

    aaaaaa

--- Assets/Scripts/teststage/arrival.cs
+++ Assets/Scripts/teststage/arrival.cs
@@ -6,25 +6,48 @@ using System;
 using System.IO.MemoryMappedFiles;
 using System.Threading;
 
-public class ReadLife : MonoBehaviour
+public class ReadArrival : MonoBehaviour
 {
     //共有メモリの読み込み
-    MemoryMappedFile lifefrombve;
-    MemoryMappedViewAccessor life;
+    //停車
+    MemoryMappedFile arrivalfrombve;
+    MemoryMappedViewAccessor arrivaltime;
+    //通過
+    MemoryMappedFile pastfrombve;
+    MemoryMappedViewAccessor pasttime;
+    //判定
+    MemoryMappedFile passfrombve;
+    MemoryMappedViewAccessor passhantei;
     public int lifetime;
+    public int pass
     public TextMeshProUGUI =textMeshPro;
     // Start is called before the first frame update
     void Start()
     {
         //BVE側から
-        MemoryMappedFile lifefrombve = MemoryMappedFile.OpenExisting("Life");
-        life = lifefrombve.CreateViewAccessor();
-        lifetime = life.ReadInt32(0);
+        //停車
+        MemoryMappedFile arrivalfrombve = MemoryMappedFile.OpenExisting("Arrival");
+        arrival = arrivalfrombve.CreateViewAccessor();
+        arrivaltime = arrival.ReadInt32(0);
+        //通過
+        MemoryMappedFile pastfrombve = MemoryMappedFile.OpenExisting("Past");
+        pasttime = pastfrombve.CreateViewAccessor();
+        past = pasttime.ReadInt32(0);
+        //通貨停車判定
+        MemoryMappedFile passfrombve = MemoryMappedFile.OpenExisting("Past");
+        pass = passhantei.CreateViewAccessor();
     }
 
     // Update is called once per frame
     void Update()
     {
-        textMeshPro.text= lifetime.ToString();//持ち時間の値をテキストに代入
+        if(pass=1)//停車
+        {
+            textMeshPro.text= arrival.ToString();//到着時刻の値をテキストに代入
+        }
+        if(pass=0)//通過
+        {
+            textMeshPro.text= arrival.ToString();
+        }
     }
 }
