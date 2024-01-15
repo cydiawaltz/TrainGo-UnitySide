d68fe699e2d3b6cd49e7592d782bad3bb5073114
cydia-waltz <135396758+cydiawaltz@users.noreply.github.com>
2024-01-15 16:30:24 +0900

    aaaaaaaa

---
+++ Assets/Scripts/teststage/arritime.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using TMPro;
+using System.IO.MemoryMappedFiles;
+
+public class arritime :MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        
+    }
+}
